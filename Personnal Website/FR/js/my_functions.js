c86c943c9de5a6901883b96b0e31911014264cde
David Bensoussan <minipada@gmail.com>
2015-07-05 16:02:04 +0200

    Correct error on hiding function

--- Personnal Website/FR/js/my_functions.js
+++ Personnal Website/FR/js/my_functions.js
@@ -1,10 +1,13 @@
 function hide_menu() {
-    "use strict";
     var currentLocation = window.location;
     
-    if ((currentLocation !== "http://davidbensoussan.olympe.in/EN/index.html") && (currentLocation !== "http://davidbensoussan.olympe.in/FR/index.html")) {
+    if ((currentLocation == "http://davidbensoussan.olympe.in/EN/index.html") && (currentLocation == "http://davidbensoussan.olympe.in/FR/index.html")) {
         document.getElementById("menu_button").style.display = "none";
         document.getElementById("download_button").style.display = "none";
+    }
+    else {
+        document.getElementById("menu_button").style.display = "inline";
+        document.getElementById("download_button").style.display = "inline";
     }
 }
 
